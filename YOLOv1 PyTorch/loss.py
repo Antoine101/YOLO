6d8ff036ff6548dfd7077246b5f08c808c7736ba
apucementys <90397822+apucementys@users.noreply.github.com>
2022-04-20 10:06:19 +0200

    First commit

---
+++ YOLOv1 PyTorch/loss.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+from utils import intersection_over_union
+
+class YOLOLoss(nn.Module):
+    def __init__(self, S=7, B=2, C=20):
+        super(YOLOLoss, self).__init__()
+        self.mse = nn.MSELoss(reduction="sum")
+        self.S = S
+        self.B = B
+        self.C = C
+        self.lambda_noobj = 0.5
+        self.lambda_coord = 5
+    
+    def forward(self, predictions, target):
+        predictions = predictions.reshape(-1, self.S, self.S, self.C + self.B * 5)
+
+        iou_b1 = intersection_over_union(predictions[..., 21:25], target[..., 21:25])
+        iou_b2 = intersection_over_union(predictions[..., 26:30], target[..., 21:25])
+        ious = torch.cat([iou_b1.unsqueeze(0), iou_b2.unsqueeze(0)], dim=0)
+        iou_max, best_box = torch.max(ious, dim=0)
+        # Iobj_1
+        exists_box = target[..., 20].unsqueeze(3)
+
+        box_predictions = exists_box * (best_box * predictions[..., 26:30] + (1 - best_box) * predictions[..., 21:25])
+        box_targets = exists_box * target[..., 21:25]
+        box_predictions[..., 2:4] = torch.sign(box_predictions[..., 2:4]) * torch.sqrt(torch.abs(box_predictions[..., 2:4] + 1e-6))
+
+        box_targets[..., 2:4] = torch.sqrt(box_targets[..., 2:4])
